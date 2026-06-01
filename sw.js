d398bdc5b7d7ca6532b45d8062a228c5127c5f5b
1232456-afk <muh.alihassan.pk@gmail.com>
2026-06-01 13:26:40 +0500

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'healthscan-v3';
+const FILES = [
+  './index.html',
+  './style.css', 
+  './app.js',
+  './manifest.json',
+  './favicon.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() =>
+      caches.match('./index.html')
+    ))
+  );
+});
